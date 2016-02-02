19282db549b8e87cf253d861aac5e477cf4d645a
Thiago Tobias <thiagotobias@gmail.com>
2016-02-02 13:08:19 -0200

    pdo parte listar

--- Aluno.php
+++ Aluno.php
@@ -1,10 +1,79 @@
 <?php
 
-class Alunos
+class Aluno
 {
+    private $db;
+
     private $id;
     private $nome;
     private $nota;
+
+    public function __construct(\PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function find($id)
+    {
+        $query = "SELECT * FROM alunos WHERE id = :id ;";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(":id", $id);
+        $stmt->execute();
+
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function listar($ordem = null)
+    {
+        if($ordem){
+            $query = "SELECT * FROM alunos ORDER BY {$ordem};";
+        }else{
+            $query = "SELECT * FROM alunos;";
+        }
+        
+        $stmt = $this->db->query($query);
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function inserir()
+    {
+        $query = "INSERT INTO alunos(nome,nota)VALUES(:nome, :nota);";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':nome',$this->getNome());
+        $stmt->bindValue(':nota',$this->getNota());
+
+        if ($stmt->execute()) {
+            return true;
+        }
+    }
+
+    public function alterar()
+    {
+        $query = "UPDATE alunos SET nome=:nome, nota=:nota WHERE id=:id;";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id',$this->getId());
+        $stmt->bindValue(':nome',$this->getNome());
+        $stmt->bindValue(':nota',$this->getNota());
+
+        if ($stmt->execute()) {
+            return true;
+        }
+    }
+
+    public function deletar($id)
+    {
+        $query = "DELETE FROM alunos WHERE id=:id;";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id',$id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+    }
 
     /**
      * @return mixed
@@ -56,8 +125,4 @@ class Alunos
         $this->nota = $nota;
         return $this;
     }
-
-
-
-
 }
--- index.php
+++ index.php
@@ -1,71 +1,47 @@
 <?php
-//require_once 'Aluno.php';
 
- try{
+require_once 'Aluno.php';
+
+try{
     $conexao = new \PDO("mysql:host=localhost;dbname=pdo","root","root");
-
-    $query = "SELECT * FROM alunos;";
-    $stmt = $conexao->prepare($query);
-    $stmt->execute();
-    $resultado = $stmt->fetchALL(PDO::FETCH_ASSOC);
-    
-    $query_nota = "SELECT * FROM alunos ORDER BY nota DESC,nome ASC LIMIT 3;";
-    $stmt = $conexao->prepare($query_nota);
-    $stmt->execute();
-    $result_nota = $stmt->fetchALL(PDO::FETCH_ASSOC);
-    
-    ?>
-    <table border="1">
-        <tr>
-            <td>Listar Tudo</td>
-            <td>As 3 Maiores Notas</td>
-        </tr>
-        <tr>
-            <td>
-            <table border="1">
-            	<tr>
-            		<td>ID</td>
-            		<td>Nome</td>
-            		<td>Nota</td>    		
-            	</tr>
-            	<?php
-            foreach ($resultado as $key => $value) {
-            	echo "<tr>";
-            	echo "<td>".$value['id']."</td>";
-            	echo "<td>".$value['nome']."</td>";
-            	echo "<td>".$value['nota']."</td>";
-            	echo "</tr>";
-            }
-            ?>
-             </table>
-             </td>
-             <td valign="top">
-                 <table border="1">
-                    <tr>
-                        <td>ID</td>
-                        <td>Nome</td>
-                        <td>Nota</td>           
-                    </tr>
-                    <?php
-                foreach ($result_nota as $keyy => $valuee) {
-                    echo "<tr>";
-                    echo "<td>".$valuee['id']."</td>";
-                    echo "<td>".$valuee['nome']."</td>";
-                    echo "<td>".$valuee['nota']."</td>";
-                    echo "</tr>";
-                }
-                ?>
-                 </table>
-             </td>
-         </tr>
-     </table>
-
-    <?php
-
-    //print_r($resultado);
-
- }catch (\PDOException $e){
+}catch (\PDOException $e){
     die("Não foi possível estabelecer a conexão com o banco de dados: Erro código: ".$e->getCode().": ".$e->getMessage());
- }
+}
 
-//$aluno = new Alunos($conexao);
+
+$aluno = new Aluno($conexao);
+
+/* Inserir 
+$aluno->setNome("Thiago Tobias 2")
+      ->setNota(8)
+;
+
+$resultado = $aluno->inserir();
+echo $resultado;
+*/
+
+/* Listar
+foreach ($aluno->listar("id DESC") as $a) {
+    echo $a['nome']."<br>";    
+}
+*/
+
+/*Alterar
+$aluno->setID(7)
+      ->setNome("Thiago Tobias 2")
+      ->setNota(8)
+;
+
+$resultado = $aluno->alterar();
+echo $resultado;
+*/
+
+/* Remover
+$resultado = $aluno->deletar(7);
+foreach ($aluno->listar("id DESC") as $a) {
+    echo $a['nome']."<br>";    
+}
+*/
+
+$resultado = $aluno->find(8);
+echo $resultado['nome']." - ".$resultado['nota'];
